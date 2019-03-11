338599ab9d69364b35f6d2c63a1a350e75666f8c
Leonardo Davinci <drv@outlook.com>
2019-03-11 22:04:40 +0000

    First commit

---
+++ precache-manifest.31584f5f9995b35b025e89b1d98a3205.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa3c1e5d56b4dc9d5e5031ca33afaf7",
+    "url": "/davidreact/portfolio/static/media/Reminder Pro.8fa3c1e5.png"
+  },
+  {
+    "revision": "8062853f1a9c339662f4",
+    "url": "/davidreact/portfolio/static/css/main.dc8b6448.chunk.css"
+  },
+  {
+    "revision": "b1c35de6c20468a0e6fe",
+    "url": "/davidreact/portfolio/static/js/runtime~main.aa5120a5.js"
+  },
+  {
+    "revision": "cdf67b258d25f5cbdf84",
+    "url": "/davidreact/portfolio/static/js/2.a5cd9056.chunk.js"
+  },
+  {
+    "revision": "1d1f5bf8953517ddc12002d095ecd131",
+    "url": "/davidreact/portfolio/static/media/Crypto Price Check.1d1f5bf8.png"
+  },
+  {
+    "revision": "8e82bd92ecf20c55ee47d6fa49eb2eda",
+    "url": "/davidreact/portfolio/static/media/nonasciicharacters.8e82bd92.png"
+  },
+  {
+    "revision": "e95588c77ee47ff4dbcc1dbe80c93e4d",
+    "url": "/davidreact/portfolio/static/media/Pomodoro Clock.e95588c7.png"
+  },
+  {
+    "revision": "8062853f1a9c339662f4",
+    "url": "/davidreact/portfolio/static/js/main.ab4ea47a.chunk.js"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/davidreact/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/davidreact/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/davidreact/portfolio/static/media/twitter_icon.512536cb.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/davidreact/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "ed9c99b8a61296e74516a0d0e1b1b8e2",
+    "url": "/davidreact/portfolio/static/media/David shot v3.ed9c99b8.jpeg"
+  },
+  {
+    "revision": "8a4996f4df3e458b58172fbd3d71bd59",
+    "url": "/davidreact/portfolio/index.html"
+  }
+];
